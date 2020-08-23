9d8415fe2ee40106fc4a3d995564bca3296d9a49
t.i <isoittech@gmail.com>
2020-08-24 08:12:35 +0900

    EventKouhoNichijiテーブルへの追加処理実装

---
+++ home/helpers.py
@@ -0,0 +1,70 @@
+import base64
+import os
+import re
+
+CRYPT_SEED_FOR_ID = 'CRYPT_SEED_FOR_ID'
+CRYPT_SEED_FOR_ANSHOU_NUM = 'CRYPT_SEED_FOR_ANSHOU_NUM'
+LINE_FEED_CD = os.linesep
+
+
+def encode(encode_key, target_str):
+    enc = []
+    for i in range(len(target_str)):
+        encode_key_c = encode_key[i % len(encode_key)]
+        enc_c = chr((ord(target_str[i]) + ord(encode_key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+
+def decode(encode_key, encoded_str):
+    dec = []
+    encoded_str = base64.urlsafe_b64decode(encoded_str).decode()
+    for i in range(len(encoded_str)):
+        encode_key_c = encode_key[i % len(encode_key)]
+        dec_c = chr((256 + ord(encoded_str[i]) - ord(encode_key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+def create_schedule_update_id(schedule_update_id_seed):
+    try:
+        return encode(CRYPT_SEED_FOR_ID, '<crypt>' + str(schedule_update_id_seed) + '</crypt>')
+    except Exception as e:
+        print('[ERROR] create_schedule_update_idでエラー。引数:{}'.format(schedule_update_id_seed))
+        raise e
+
+
+def encode_anshou_num(anshou_num):
+    try:
+        return encode(CRYPT_SEED_FOR_ANSHOU_NUM, '<crypt>' + str(anshou_num) + '</crypt>')
+    except Exception as e:
+        print('[ERROR] encode_anshou_numでエラー。引数:{}'.format(anshou_num))
+        raise e
+
+
+def decode_from_schedule_update_id(schedule_update_id):
+    try:
+        decoded = decode(CRYPT_SEED_FOR_ID, schedule_update_id)
+        match = re.findall(r'<crypt>(.*)</crypt>', decoded)
+        return match[0]
+    except Exception as e:
+        print('[ERROR] decode_from_schedule_update_idでエラー。引数:{}'.format(schedule_update_id))
+        raise e
+
+
+def decode_from_encoded_anshou_num(encoded_anshou_num):
+    try:
+        decoded = decode(CRYPT_SEED_FOR_ANSHOU_NUM, encoded_anshou_num)
+        match = re.findall(r'<crypt>(.*)</crypt>', decoded)
+        return match[0]
+    except Exception as e:
+        print('[ERROR] decode_from_encoded_anshou_numでエラー。引数:{}'.format(encoded_anshou_num))
+        raise e
+
+
+def get_event_datetime_kouhos(event_datetime_kouho_str):
+    try:
+        return event_datetime_kouho_str.split(LINE_FEED_CD)
+    except Exception as e:
+        print('[ERROR] get_event_datetime_kouhosでエラー。引数:{}'.format(event_datetime_kouho_str))
+        raise e
--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
+from django.db import transaction
 from django.shortcuts import render, get_object_or_404, redirect
 
+from home import helpers
 from home.forms import EventForm
 from home.models import Event, EventKouhoNichiji
 
 
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+# トップページ遷移関数
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 def index(request):  # 追加
     """イベントの一覧"""
     events = Event.objects.all().order_by('id')
@@ -11,16 +16,71 @@ def index(request):  # 追加
                   'home/top.html')
 
 
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+# イベント追加関数
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 def event_add(request):
     """イベントの追加"""
-    event = Event()
-    eventKouhoNichiji = EventKouhoNichiji()
-
     form = EventForm(request.POST)  # POST された request データからフォームを作成
     if form.is_valid():  # フォームのバリデーション
-        event = form.save(commit=False)
-        eventKouhoNichiji.kouho_nichiji
-        event.save()
+        with transaction.atomic():
+            # ===================================================
+            # 保存対象モデルデータ作成
+            # ※Eventはフォームデータから作成する。
+            # ===================================================
+            event = form.save(commit=False)
+
+            # ===================================================
+            # 保存対象データ設定処理
+            # ===================================================
+            # ------------------------
+            # Event
+            # ------------------------
+            # 暗証番号
+            # ※暗号化して保存する。
+            encoded_anshou_num = helpers.encode_anshou_num(form.cleaned_data['anshou_num'])
+            event.anshou_num = encoded_anshou_num
+
+            # ===================================================
+            # 保存処理 #1
+            # ===================================================
+            event.save()
+
+            # ===================================================
+            # ここから保存処理#2用処理
+            # ===================================================
+
+            # ===================================================
+            # イベント日時候補レコード作成処理
+            # 　画面の「イベント日時候補」※に入力された文字列を取得、処理して保存対象データを加工・作成する。
+            # 　※改行コード区切り
+            # ===================================================
+            event_datetime_kouhos = helpers.get_event_datetime_kouhos(form.cleaned_data['event_datetime_kouho'])
+
+            # ===================================================
+            # 保存対象データ設定処理
+            # ===================================================
+            # ------------------------
+            # Event
+            # ------------------------
+            # スケジュール更新ページID
+            schedule_update_id = helpers.create_schedule_update_id(event.id)
+            event.schedule_update_id = schedule_update_id
+
+            # ------------------------
+            # EventKouhoNichiji
+            # ------------------------
+            eventKouhoNichijis = [EventKouhoNichiji(event=event, kouho_nichiji=event_datetime_kouho) for event_datetime_kouho in event_datetime_kouhos]
+
+            # ===================================================
+            # 保存処理 #2
+            # ===================================================
+            EventKouhoNichiji.objects.bulk_create(eventKouhoNichijis)
+            event.save()
+
+            # ===================================================
+            # 次画面遷移
+            # ===================================================
         return render(request,
                       'home/schedule_fill.html',  # 使用するテンプレート
                       dict(form=form, event_id=event.id))  # テンプレートに渡すデータ
@@ -28,6 +88,10 @@ def event_add(request):
     return render(request, 'home/top.html', dict(form=form))
     # return redirect('home:index')
 
+
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+# スケジュール更新関数
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 def schedule_fill(request):
     """イベントの一覧"""
     events = Event.objects.all().order_by('id')
@@ -36,6 +100,9 @@ def schedule_fill(request):
                   {'events': events})  # テンプレートに渡すデータ
 
 
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+# イベント一覧化関数
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 def event_list(request):
     """イベントの一覧"""
     events = Event.objects.all().order_by('id')
@@ -44,8 +111,11 @@ def event_list(request):
                   {'events': events})  # テンプレートに渡すデータ
 
 
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+# イベント追加・編集関数
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 def event_edit(request, event_id=None):
-    """書籍の編集"""
+    """イベントの編集"""
     # return HttpResponse('書籍の編集')
     if event_id:  # event_id が指定されている (修正時)
         event = get_object_or_404(Event, pk=event_id)
@@ -64,6 +134,9 @@ def event_edit(request, event_id=None):
     return render(request, 'home/event_edit.html', dict(form=form, event_id=event_id))
 
 
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+# イベント削除関数
+# ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 def event_del(request, event_id):
     """イベントの削除"""
     # return HttpResponse('イベントの削除')
