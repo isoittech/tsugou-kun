260d72c4dc89a51d40a0061689f4e05b22168789
t.i <isoittech@gmail.com>
2021-04-21 22:13:28 +0900

    初期コマンド省略化再トライ

--- app/container_init.sh
+++ app/container_init.sh
@@ -1,8 +1,7 @@
 if [ ! -e '/check' ]; then
     touch /check
     # 初回起動時実行コマンド
-    gunicorn app.wsgi:application --bind 0.0.0.0:8000
-    python ./manage.py collectstatic
+    echo "yes" | python ./manage.py collectstatic
     python manage.py migrate --noinput
 else
     # 2回目以降
